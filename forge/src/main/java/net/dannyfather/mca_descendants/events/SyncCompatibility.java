2c28a815eb877dc916dcd185f01fc3f5afc6d6ff
DannyFather <danlevibeb@gmail.com>
2026-04-12 20:02:51 +0800

    Update 1.0.1

--- forge/src/main/java/net/dannyfather/mca_descendants/events/SyncCompatibility.java
+++ forge/src/main/java/net/dannyfather/mca_descendants/events/SyncCompatibility.java
@@ -30,6 +30,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import static net.dannyfather.mca_descendants.events.MCADescendantsEvents.*;
 import static net.dannyfather.mca_descendants.network.c2s.getDescendantsRequest.getGrandchildren;
 
 
@@ -50,11 +51,21 @@ public class SyncCompatibility {
                     }
 
                     if (shellCount == 0) {
-                        player.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY, -1, 0, false, false));
-                        Entity soul = ModUtils.summonSoul(player, serverLevel);
-                        soul.moveTo(player.blockPosition(), player.getYRot(), player.getXRot());
+                        FamilyTree tree = FamilyTree.get(serverLevel);
+                        FamilyTreeNode playerNode = tree.getOrEmpty(player.getUUID()).get();
+                        int childrenCount = playerNode.children().size();
+                        CHILDREN_COUNT.put(player.getUUID(),childrenCount);
+                        int grandchildrenCount = getGrandchildren(playerNode,serverLevel).size();
+                        GRANDCHILDREN_COUNT.put(player.getUUID(),grandchildrenCount);
+                        String deathMsg = event.getSource().getLocalizedDeathMessage(player).getString();
+                        LAST_DEATH_MESSAGE.put(player.getUUID(),deathMsg);
+                        String villagerName = PlayerSaveData.get(player).getEntityData().getString("villagerName");
+                        LAST_VILLAGER_NAME.put(player.getUUID(),villagerName);
+                        player.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY,-1,0,false,false));
+                        Entity soul = ModUtils.summonSoul(player,serverLevel);
+                        soul.moveTo(player.blockPosition(),player.getYRot(),player.getXRot());
                         serverLevel.addFreshEntity(soul);
-                        ModUtils.evilSwapVillagerAndPlayer(((LivingEntity) soul), player);
+                        ModUtils.evilSwapVillagerAndPlayer(((LivingEntity) soul),player);
                     }
 
                 }
