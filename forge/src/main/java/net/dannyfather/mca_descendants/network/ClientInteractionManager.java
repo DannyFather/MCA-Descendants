77ccb366e526f9cc92c148356845b082871ee451
DannyFather <danlevibeb@gmail.com>
2026-04-13 22:54:40 +0800

    Update 1.0.2

--- forge/src/main/java/net/dannyfather/mca_descendants/network/ClientInteractionManager.java
+++ forge/src/main/java/net/dannyfather/mca_descendants/network/ClientInteractionManager.java
@@ -1,10 +1,13 @@
 package net.dannyfather.mca_descendants.network;
 
+import net.dannyfather.mca_descendants.client.gui.PhoneScreen;
 import net.dannyfather.mca_descendants.network.s2c.OpenGuiRequest;
 import net.dannyfather.mca_descendants.network.s2c.getDescendantResponse;
+import net.minecraft.client.Minecraft;
 
 public interface ClientInteractionManager {
-    void handleGuiRequest(OpenGuiRequest message);
-
+    static void handle(OpenGuiRequest msg) {
+        Minecraft.getInstance().setScreen(new PhoneScreen(msg.villager));
+    }
     void handleDescendantDataResponse(getDescendantResponse message);
 }
