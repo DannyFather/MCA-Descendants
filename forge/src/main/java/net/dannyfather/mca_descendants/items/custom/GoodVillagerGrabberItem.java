3b85ef77e73aeb7d398446ff0f7b47e9a2e2af90
DannyFather <danlevibeb@gmail.com>
2026-04-13 22:51:49 +0800

    Update GoodVillagerGrabberItem.java

--- forge/src/main/java/net/dannyfather/mca_descendants/items/custom/GoodVillagerGrabberItem.java
+++ forge/src/main/java/net/dannyfather/mca_descendants/items/custom/GoodVillagerGrabberItem.java
@@ -1,26 +1,15 @@
 package net.dannyfather.mca_descendants.items.custom;
 
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.GameType;
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.scores.Scoreboard;
-import net.minecraft.world.scores.Team;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.UUID;
-
 import static net.dannyfather.mca_descendants.util.ModUtils.goodSwapVillagerAndPlayer;
-import static net.dannyfather.mca_descendants.util.ModUtils.swapVillagerAndPlayer;
-
-
 public class GoodVillagerGrabberItem extends Item {
 
     public GoodVillagerGrabberItem(Properties pProperties) {
@@ -29,7 +18,9 @@ public class GoodVillagerGrabberItem extends Item {
 
     @Override
     public @NotNull InteractionResult interactLivingEntity(ItemStack pStack, Player pPlayer, LivingEntity target, InteractionHand hand) {
-        goodSwapVillagerAndPlayer(target,pPlayer);
+        if(pPlayer instanceof ServerPlayer serverPlayer) {
+            goodSwapVillagerAndPlayer(target, serverPlayer);
+        }
         return InteractionResult.SUCCESS;
     }
 }
