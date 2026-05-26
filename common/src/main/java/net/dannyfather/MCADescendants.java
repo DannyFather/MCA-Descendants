5904af01c61355a4a0197d68fe9fb8f59495ea5c
DannyFather <danlevibeb@gmail.com>
2026-05-26 22:51:36 +0800

    Update 1.1.0

---
+++ common/src/main/java/net/dannyfather/MCADescendants.java
@@ -0,0 +1,13 @@
+package net.dannyfather;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class MCADescendants {
+    public static final String MOD_ID = "mca_descendants";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static void init() {
+    }
+}
