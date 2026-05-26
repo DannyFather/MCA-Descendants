921acf6e4343c05752d5dce60d1788807bd77907
DannyFather <danlevibeb@gmail.com>
2026-05-26 22:55:45 +0800

    Update 1.1.0

---
+++ fabric/src/main/java/net/dannyfather/fabric/MCADescendantsFabric.java
@@ -0,0 +1,17 @@
+package net.dannyfather.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.dannyfather.MCADescendants;
+
+public final class MCADescendantsFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        // Run our common setup.
+        MCADescendants.init();
+    }
+}
---
+++ fabric/src/main/java/net/dannyfather/fabric/client/MCADescendantsFabricClient.java
@@ -0,0 +1,10 @@
+package net.dannyfather.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class MCADescendantsFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
